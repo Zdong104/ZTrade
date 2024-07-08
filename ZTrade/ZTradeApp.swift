c812aa79bf0ef24ce30c0b065b237b1dc7a70624
Zihan Dong <puma122707@gmail.com>
2024-07-08 14:04:15 -0400

    Initial Commit

---
+++ ZTrade/ZTradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZTradeApp.swift
+//  ZTrade
+//
+//  Created by Zihan Dong on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ZTradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
